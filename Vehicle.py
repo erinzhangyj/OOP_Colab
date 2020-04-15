82500b1d4cb3899ecf0db32f143bd7ef652e83f8
ian <ianhojy@gmail.com>
2020-04-16 01:21:50 +0800

    More Vehicle Methods

--- Vehicle.py
+++ Vehicle.py
@@ -1,3 +1,32 @@
 class Vehicle:
 
-    def __init__(self):
+    FUEL_PER_DIST = None
+
+    def __init__(self, init_fuel):
+        self.fuel = init_fuel
+        self.total_distance = 0
+
+    def move(self, distance):
+        self.total_distance += distance
+        self.fuel -= self.FUEL_PER_DIST * distance
+
+    def add_fuel(self, amount):
+        self.fuel += amount
+
+
+class Car(Vehicle):
+
+    FUEL_PER_DIST = 1
+
+    def __init__(self, init_fuel):
+        super().__init__(init_fuel)
+
+
+if __name__ == '__main__':
+    c = Car(29)
+    print(c.fuel)
+    c.move(10)
+    print(c.fuel)
+
+
+
